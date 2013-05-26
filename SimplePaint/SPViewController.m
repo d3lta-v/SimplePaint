ba9972eb91af0ad5221f3fb9a135af95b1e69cfd
Pan Ziyue <random.rrr3@gmail.com>
2013-05-26 11:36:44 +0800

    allowed drawing inside of rect

--- SimplePaint/SPViewController.m
+++ SimplePaint/SPViewController.m
@@ -20,10 +20,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    nestedView.backgroundColor=[UIColor colorWithWhite:0 alpha:0]; //This is to make the nested view transparent
+    //nestedView.backgroundColor=[UIColor colorWithWhite:0 alpha:0]; //This is to make the nested view transparent
 
-    SPView *paint = [[SPView alloc] initWithFrame:nestedView.bounds]; //original code is SPView *paint=[[SPView alloc] initWithFrame:self.view.bounds];
-    [nestedView addSubview:paint]; //original code is [self.view addSubview:paint];
+    //SPView *paint = [[SPView alloc] initWithFrame:nestedView.bounds]; //original code is SPView *paint=[[SPView alloc] initWithFrame:self.view.bounds];
+    //[nestedView addSubview:paint]; //original code is [self.view addSubview:paint];
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    
+    UIView *newView = [[UIView alloc] initWithFrame:self.view.bounds]; //This is the entire thing
+    newView.backgroundColor = [UIColor grayColor]; //This is for the color of the entire thing
+    
+    SPView *newPaint = [[SPView alloc] initWithFrame:CGRectInset(self.view.bounds, 40,40)]; //Specific dimensions here
+    newPaint.backgroundColor=[UIColor colorWithWhite:1 alpha:1];
+    
+    [newView addSubview:newPaint]; //newView is essentially self, and newPaint is the initWithFrame made programmatically
+    
+    self.view = newView;
 }
 
 - (void)didReceiveMemoryWarning
